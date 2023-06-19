9140600c200b17691790f5b4a319a7d511d9fd90
unknown <mgehring9@gmail.com>
2023-06-19 10:52:25 -0700

    clean up formatting

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ fetch('videos.json')
   function calculateAndDisplayVideoSummary() {
     let totalHours = {};
     let overallHours = 0;
-    const levels = ["superbeginner", "beginner", "intermediate", "advanced"];
+    const levels = ["Superbeginner", "Beginner", "Intermediate", "Advanced"];
   
     data.videos.forEach(video => {
       const level = video.level;
@@ -28,15 +28,15 @@ fetch('videos.json')
     summaryDiv.innerHTML = '';
   
     levels.forEach(level => {
-      if (totalHours[level]) {
+      if (totalHours[level.toLowerCase()]) {
         let p = document.createElement('p');
-        p.textContent = `Level: ${level}, Total hours: ${totalHours[level].toFixed(2)}`;
+        p.textContent = `Level: ${level}, Total hours: ${totalHours[level.toLowerCase()].toFixed(0)}`;
         summaryDiv.appendChild(p);
       }
     });
   
     let overallP = document.createElement('p');
-    overallP.textContent = `Overall hours across all levels: ${overallHours.toFixed(2)}`;
+    overallP.textContent = `Overall hours across all levels: ${overallHours.toFixed(0)}`;
     summaryDiv.appendChild(overallP);
   }
   
@@ -48,16 +48,31 @@ function searchVideos() {
 }
 
 function displaySearchResults(results) {
-  let resultsDiv = document.getElementById('searchResults');
-  resultsDiv.innerHTML = '';
+  let searchResultsDiv = document.getElementById('searchResults');
+  searchResultsDiv.innerHTML = '';
 
-  results.forEach(video => {
-    let link = document.createElement('a');
-    link.textContent = video.title;
-    link.href = `https://www.dreamingspanish.com/watch?id=${video._id}`;
-    resultsDiv.appendChild(link);
+  // Sort results by level
+  let sortedResults = results.sort((a, b) => {
+    const levels = ["superbeginner", "beginner", "intermediate", "advanced"];
+    return levels.indexOf(a.level) - levels.indexOf(b.level);
+  });
 
-    let br = document.createElement('br');
-    resultsDiv.appendChild(br);
+  let currentLevel = null;
+
+  sortedResults.forEach(result => {
+    if (result.level !== currentLevel) {
+      // New level, add a heading
+      let h3 = document.createElement('h3');
+      const capitalized = result.level.charAt(0).toUpperCase() + result.level.slice(1);
+      h3.textContent = `${capitalized}`;
+      searchResultsDiv.appendChild(h3);
+      currentLevel = result.level;
+    }
+
+    // Add the video link
+    let a = document.createElement('a');
+    a.href = `https://www.dreamingspanish.com/watch?id=${result._id}`;
+    a.textContent = `${result.title} - Duration: ${Math.round(result.duration / 60)} minutes`;
+    searchResultsDiv.appendChild(a);
   });
 }
